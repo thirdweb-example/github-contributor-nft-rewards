a42e895546d7d56d274a5f5f7543864a0085da5d
Jarrod Watts <jarrodwatts16@gmail.com>
2022-08-30 11:42:45 +1000

    Add connect wallet button and styles to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { useLogin } from "@thirdweb-dev/react";
+import { useAddress, useLogin, useMetamask } from "@thirdweb-dev/react";
 import { useSession, signIn } from "next-auth/react";
+import styles from "../styles/Home.module.css";
 
 export default function Login() {
   const login = useLogin();
   const { data: session } = useSession();
+  const address = useAddress();
+  const connect = useMetamask();
 
   return (
-    <div>
-      {!session ? (
-        <button onClick={() => signIn()}>Sign in</button>
+    <div className={styles.container}>
+      {!address ? (
+        <button className={styles.mainButton} onClick={() => connect()}>
+          Connect Wallet
+        </button>
+      ) : !session ? (
+        <button className={styles.mainButton} onClick={() => signIn()}>
+          Sign in
+        </button>
       ) : (
-        <button onClick={() => login()}>Sign in with Ethereum</button>
+        <button className={styles.mainButton} onClick={() => login()}>
+          Sign in with Ethereum
+        </button>
       )}
     </div>
   );
